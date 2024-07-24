09958eb14876bd416775ddff9839b257c1d9882e
gaurizarekar <gaurizarekar14@gmail.com>
2024-07-24 23:23:17 +0530

    new chnges

---
+++ aos.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(".heading").waypoint(function(){
+        $(".heading").addClass("animate__animated animate__fadeInLeft")
+    },{offset:"80%"});
+    $(".line").waypoint(function(){
+        $(".line").addClass("animate__animated animate__fadeInUp")
+    },{offset:"80%"});
+    $(".about").waypoint(function(){
+        $(".about").addClass("animate__animated animate__fadeInUp")
+    },{offset:"80%"});
+    $(".projects").waypoint(function(){
+        $(".projects").addClass("animate__animated animate__fadeInUp")
+    },{offset:"80%"});
+    $(".contact-box").waypoint(function(){
+        $(".contact-box").addClass("animate__animated animate__fadeInUp")
+    },{offset:"80%"});
+  
+})
